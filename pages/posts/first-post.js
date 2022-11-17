3846460ac289b44f1851f720b167ab577f89c6b8
briangor <briangor13@gmail.com>
2022-11-17 23:16:32 +0300

    add html head to component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from "next/link"
+import Head from "next/head";
 
 export default function FirstPost() {
     return (
         <>
+        <Head>
+            <title>First Post</title>
+        </Head>
             <h1>Fist Post</h1>
             <h2>
                 <Link href='/'>Go back to home</Link>
