4820d48317478765630fa11ef9a991fa07d889e0
briangor <briangor13@gmail.com>
2023-01-22 15:29:00 +0300

    update post layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,21 @@
 import Link from "next/link"
 import Head from "next/head";
+// import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
-        <Head>
-            <title>First Post .</title>
-        </Head>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            {/* <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnLoad"
+                onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
+            /> */}
             <h1>Fist Post</h1>
-            <h2>
-                <Link href='/'>Go back to home</Link>
-            </h2>
-        </>
+            
+        </Layout>
     );
 }
